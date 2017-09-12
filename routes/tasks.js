993b9eaa100a4dc994555a5a8ead84da0d4b6d14
George Gusein <georgegusein@gmail.com>
2017-09-11 17:58:27 -0700

    db connected

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://virtnet:1234@ds153413.mlab.com:53413/virtnet',['tasks']);
 router.get('/tasks',(req,res,next)=>{
-    res.send('task api');
+    db.tasks.find((err,tasks)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.json(tasks);
+        }
+    });
 });
 
 module.exports = router;
